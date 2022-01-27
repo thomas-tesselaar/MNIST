b2086d819e13e16cc50084b56012623c21ba2f4e
thomas-tesselaar <72113407+thomas-tesselaar@users.noreply.github.com>
2022-01-27 03:57:22 -0500

    Create logistic-regression.py

---
+++ logistic-regression.py
@@ -0,0 +1,89 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+img_h = img_w = 28             # MNIST images are 28x28
+img_size_flat = img_h * img_w  # 28x28=784, the total number of pixels
+n_classes = 10                 # Number of classes, one class per digit
+
+def load_data(mode='train'):
+    from tensorflow.examples.tutorials.mnist import input_data
+    mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+    if mode == 'train':
+        x_train, y_train, x_valid, y_valid = mnist.train.images, mnist.train.labels, \
+                                             mnist.validation.images, mnist.validation.labels
+        return x_train, y_train, x_valid, y_valid
+    elif mode == 'test':
+        x_test, y_test = mnist.test.images, mnist.test.labels
+    return x_test, y_test
+
+def randomize(x, y):
+    permutation = np.random.permutation(y.shape[0])
+    shuffled_x = x[permutation, :]
+    shuffled_y = y[permutation]
+    return shuffled_x, shuffled_y
+
+def get_next_batch(x, y, start, end):
+    x_batch = x[start:end]
+    y_batch = y[start:end]
+    return x_batch, y_batch
+# Load MNIST data
+x_train, y_train, x_valid, y_valid = load_data(mode='train')
+
+# Hyper-parameters
+epochs = 10             # Total number of training epochs
+batch_size = 100        # Training batch size
+display_freq = 100      # Frequency of displaying the training results
+learning_rate = 0.001   # The optimization initial learning rate
+
+def weight_variable(shape):
+    initer = tf.truncated_normal_initializer(stddev=0.01)
+    return tf.get_variable('W',
+                           dtype=tf.float32,
+                           shape=shape,
+                           initializer=initer)
+
+def bias_variable(shape):
+    initial = tf.constant(0., shape=shape, dtype=tf.float32)
+    return tf.get_variable('b',
+                           dtype=tf.float32,
+                           initializer=initial)
+sess = tf.InteractiveSession()
+# Initialize all variables
+sess.run(init)
+# Number of training iterations in each epoch
+num_tr_iter = int(len(y_train) / batch_size)
+for epoch in range(epochs):
+    print('Training epoch: {}'.format(epoch + 1))
+    # Randomly shuffle the training data at the beginning of each epoch 
+    x_train, y_train = randomize(x_train, y_train)
+    for iteration in range(num_tr_iter):
+        start = iteration * batch_size
+        end = (iteration + 1) * batch_size
+        x_batch, y_batch = get_next_batch(x_train, y_train, start, end)
+
+        # Run optimization op (backprop)
+        feed_dict_batch = {x: x_batch, y: y_batch}
+        sess.run(optimizer, feed_dict=feed_dict_batch)
+
+        if iteration % display_freq == 0:
+            # Calculate and display the batch loss and accuracy
+            loss_batch, acc_batch = sess.run([loss, accuracy],
+                                             feed_dict=feed_dict_batch)
+
+            print("iter {0:3d}:\t Loss={1:.2f},\tTraining Accuracy={2:.01%}".
+                  format(iteration, loss_batch, acc_batch))
+
+    # Run validation after every epoch
+    feed_dict_valid = {x: x_valid[:1000], y: y_valid[:1000]}
+    loss_valid, acc_valid = sess.run([loss, accuracy], feed_dict=feed_dict_valid)
+    print("Epoch: {0}, validation loss: {1:.2f}, validation accuracy: {2:.01%}".
+          format(epoch + 1, loss_valid, acc_valid))
+# Test the network after training
+# Accuracy
+x_test, y_test = load_data(mode='test')
+feed_dict_test = {x: x_test[:1000], y: y_test[:1000]}
+loss_test, acc_test = sess.run([loss, accuracy], feed_dict=feed_dict_test)
+print('---------------------------------------------------------')
+print("Test loss: {0:.2f}, test accuracy: {1:.01%}".format(loss_test, acc_test))
+print('---------------------------------------------------------')
